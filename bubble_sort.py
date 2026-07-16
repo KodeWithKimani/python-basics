6dd0b8eaa0bd2bfe6b8479c46bf50f70f0ad057f
KodeWithKimani <kodewithkimani@gmail.com>
2026-07-16 12:56:39 +0300

    16/07/2026

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,6 +3,7 @@ name = input("What is your name: ")
 def myfun():
 
     global mylist
+    global digits
 
     digits = int(input("Hi " + name + ", how many numbers do you want to try sorting? "))
     mylist = []
@@ -23,3 +24,20 @@ while confirm != "yes":
     confirm = input("Is your list correct? (yes/no): ").lower()
 
 print("We can proceed with the Bubble Sorting Algorithm")
+
+def sort_fun():
+
+    for i in range(len(mylist) - 1):
+
+        for x in range(len(mylist) - 1):
+
+            if mylist[x] > mylist[x + 1]:
+
+                temp = mylist[x]
+                mylist[x] = mylist[x + 1]
+                mylist[x + 1] = temp
+
+    print("Your sorted list is:", mylist)
+        
+
+sort_fun()
