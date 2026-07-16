ffc2185972c4d88f84d308a61ed5c9430900ad46
KodeWithKimani <kodewithkimani@gmail.com>
2026-07-16 14:01:06 +0300

    16/07/2026

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+myNum = int(input("Enter Number: "))
+
+if myNum % 2 == 0:
+    print(myNum, "is even")
+else:
+    print(myNum, "is odd")
--- prime_number_checker.py
+++ prime_number_checker.py
@@ -1,5 +1,17 @@
 myNum = int(input("Enter Number: "))
-checker = int(myNum/2)
 
-for i in range(1,checker):
-        print(i)
+# is_prime checks whether the condition is true or not
+is_prime = True
+
+# use for loop to divide each number from 2 to the number chose and checker whether modulus is 0
+# if it's 0 then it's not prime
+# use myNum//2 + 1  to shorten the range since it'll give te same result
+for divisor in range(2, myNum//2 + 1):
+    if myNum % divisor == 0:
+        is_prime = False
+        break
+
+if is_prime:
+    print(myNum, "is a prime number")
+else:
+    print(myNum, "is a composite number")
